364d4aadf4466aaaf42bd8ea5c1ec2344d83ba69
priyanshitaneja <tanejapriyanshi1997@gmail.com>
2023-06-21 06:51:47 +0530

    optimisation changes

--- src/components/Seconds.jsx
+++ src/components/Seconds.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Seconds = ({ sec }) => {
   console.log("sec rendered");
-  return <h1> {sec} </h1>;
+  return <h1> {sec.toString().padStart(2, '0')} </h1>;
 };
 
 export default React.memo(Seconds);
